484b5d0673dc5ede2edee8213532cd28cf60ea2f
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-01 18:50:27 +0200

    Implemented AsyncImageListener and handled progress bar visibility when downloading image from URL

--- app/src/main/java/mchehab/com/asynctaskdemo/MainActivity.kt
+++ app/src/main/java/mchehab/com/asynctaskdemo/MainActivity.kt
@@ -1,27 +1,46 @@
 package mchehab.com.asynctaskdemo
 
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity(), AsyncListener {
+class MainActivity : AppCompatActivity(), AsyncListener, AsyncImageListener {
 
     lateinit var textView: TextView
+    lateinit var imageView: ImageView
+    lateinit var progressBar: ProgressBar
     val URL = "http://validate.jsontest.com/?json=%7B%22key%22:%22value%22%7D"
+    val IMAGE_URL = "https://www.w3schools.com/bootstrap/paris.jpg"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         textView = findViewById(R.id.textView)
+        imageView = findViewById(R.id.imageView)
+        progressBar = findViewById(R.id.progressBar)
+
         val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener({
-            GetJSON(this).execute(URL)
-        })
+        val buttonImage = findViewById<Button>(R.id.buttonImage)
+
+        button.setOnClickListener{ GetJSON(this).execute(URL) }
+        buttonImage.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            AsyncImageDownload(this).execute(IMAGE_URL)
+        }
     }
 
     override fun getResult(result: String?) {
         textView.text = result
+    }
+
+    override fun getBitmap(bitmap: Bitmap?) {
+        progressBar.visibility = View.GONE
+        imageView.setImageBitmap(bitmap)
     }
 }

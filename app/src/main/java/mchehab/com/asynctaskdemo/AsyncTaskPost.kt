7e9a837f1e503fdf49eb46614520f7b1014f606d
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-06 19:21:33 +0200

    Replaced Buffer code with its equivalent lambda expression

--- app/src/main/java/mchehab/com/asynctaskdemo/AsyncTaskPost.kt
+++ app/src/main/java/mchehab/com/asynctaskdemo/AsyncTaskPost.kt
@@ -4,10 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.AsyncTask
 import android.support.v4.content.LocalBroadcastManager
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
 import java.lang.ref.WeakReference
 import java.net.HttpURLConnection
 import java.net.URL
@@ -25,15 +21,11 @@ broadcastIntent: String): AsyncTask<String, Int, String>() {
             httpURLConnection.requestMethod = "POST"
             httpURLConnection.connect()
 
-            val bufferedWriter = BufferedWriter(OutputStreamWriter(httpURLConnection.outputStream))
-            bufferedWriter.write(postData)
-            bufferedWriter.flush()
-            bufferedWriter.close()
+            httpURLConnection.outputStream.bufferedWriter().use { it.write(postData); it.flush() }
 
             val responseCode = httpURLConnection.responseCode
             if(responseCode == 200){
-                val bufferedReader = BufferedReader(InputStreamReader(httpURLConnection.inputStream))
-                return bufferedReader.use { it.readText() }
+                return httpURLConnection.inputStream.bufferedReader().use { it.readText() }
             }
         }catch (exception: Exception){
             exception.printStackTrace()
